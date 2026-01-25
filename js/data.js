dd10afb340da1d0c10ebab72efbdefae207e00e8
ayushraj706 <ayushrajayushhh@gmail.com>
2026-01-25 09:20:46 +0530

    data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 // Tumhara saara PDF aur Chapter data yahan rahega
 window.ayu = { 
-    sst:[{name:"2020 Set A",file:"sst1.html"},{name:"2020 Set B",file:"sst2.html"},{name:"2021 Set A",file:"sst3.html"},{name:"2021 Set B",file:"sst4.html"} {name:"2022 Set A",file:"sst5.html"}{name:"2023 Set B",file:"sst6.html"}
+    sst:[{name:"2020 Set A",file:"sst1.html"},{name:"2020 Set B",file:"sst2.html"},{name:"2021 Set A",file:"sst3.html"},{name:"2021 Set B",file:"sst4.html"} {name:"2022 Set A",file:"sst5.html"}{name:"2023 Set B",file:"sst6.html"}]
     science:[{name:"2020 Set A",file:"sci1.html"},{name:"2024 Set A",file:"sci9.html"}], 
     math:[{name:"2020 Set A",file:"mat1.html"},{name:"2024 Set A",file:"mat9.html"}], 
     gad:[{name:"Ch 1",file:"gad1.html"},{name:"Ch 2",file:"gad2.html"}], 
