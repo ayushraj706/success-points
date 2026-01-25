1cd5f50e8d5659a12d2520e2240a9b9f63be3727
ayushraj706 <ayushrajayushhh@gmail.com>
2026-01-25 10:44:34 +0530

    data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,5 @@
-// Tumhara // js/data.js
+// js/data.js
 window.ayu = {
-    // SST Section (2020 to 2026)
     sst: [
         { name: "2020 Set A", file: "sst1.html" },
         { name: "2020 Set B", file: "sst2.html" },
@@ -18,7 +17,6 @@ window.ayu = {
         { name: "2026 Set B", file: "sst14.html" }
     ],
 
-    // Science Section (2020 to 2026)
     science: [
         { name: "2020 Set A", file: "sci1.html" },
         { name: "2020 Set B", file: "sci2.html" },
@@ -36,25 +34,20 @@ window.ayu = {
         { name: "2026 Set B", file: "sci14.html" }
     ],
 
-    // Math Section
     math: [
         { name: "2020 Set A", file: "mat1.html" },
         { name: "2024 Set A", file: "mat9.html" }
     ],
 
-    // Hindi/Other (Gad)
     gad: [
         { name: "Hindi Ch 1", file: "gad1.html" },
         { name: "Hindi Ch 2", file: "gad2.html" }
     ],
 
-    // Exam Timer Date (BSEB 17 Feb 2026)
     examDate: "2026-02-17T10:00:00"
 };
 
-// Flashcards Section
 window.flashData = [
     { q: "Social Science ka Janak kise kaha jata hai?", a: "Auguste Comte" },
     { q: "Science me total kitne chapters hain?", a: "Check your new 2026 syllabus" }
 ];
- 
